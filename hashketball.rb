9e2a9c4e574970d5fd242e21d6f27bca99eef943
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-07-01 20:59:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,14 @@
 # Write your code here!
+def game_hash {
+  home: {
+    team_name:"",
+    colors: [], 
+    players:[{}]
+  },
+  away: {
+    team_name:"",
+    colors: [], 
+    players:[{}]
+  }
+}
+  
