536715963661c34b542ffd23ef0ee583acc30345
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-07-01 21:00:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,18 @@ def game_hash {
   home: {
     team_name:"",
     colors: [], 
-    players:[{}]
+    players:[
+      {
+        player_name:
+        number:
+        shoe:
+        points:
+        rebounds:
+        assists:
+        steals:
+        blocks:
+        slam_dunks:
+      }]
   },
   away: {
     team_name:"",
